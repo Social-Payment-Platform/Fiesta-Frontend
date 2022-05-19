e3a3488c3b0a801e8a27dda6684a9f8805865c68
Moyinoluwa Adenuga <dayoh14@gmail.com>
2022-05-19 08:00:03 +0100

    worked on find professional page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from './pages/login'
-import './css/style.css';
+import Home from './pages/home'
+import FindProfessional from './pages/findProfessional'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
 
 function App() {
   return (
     <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={Login} />
-    </Switch>
+      <React.StrictMode>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/sign-in" component={Login} />
+          <Route path="/find-professional" component={FindProfessional} />
+        </Switch>
+      </React.StrictMode>
   </BrowserRouter>
   );
 }
